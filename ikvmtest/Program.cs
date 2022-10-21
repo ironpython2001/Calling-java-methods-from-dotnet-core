10546d7294c32bc6524114a38ca3dbba36574447
Sunil Pottumuttu <spottumuttu@osius.com>
2022-10-21 13:02:00 +0530

    initial version

---
+++ ikvmtest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ikvmtest;
+using sample;
+
+namespace ikvmtest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloworldImpl helloworld = new HelloworldImpl();
+            Console.Write( helloworld.sayHello("sdafa"));
+        }
+    }
+}
